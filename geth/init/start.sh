4e61e9fa9438f9b12d867ee5b77a6e4d49f6beeb
clearcodecn <git@clearcode.cn>
2019-02-17 23:20:11 +0800

    push

--- geth/init/start.sh
+++ geth/init/start.sh
@@ -7,7 +7,6 @@ geth init /init/init.json
 bootnode --genkey=boot.key
 bootnode --nodekey=boot.key & 
 
-# 启动
-geth
+geth --bootnodes=enode://cc5a17554a9378fcb882b35906c01624290f7bf941eb22b2add49fa09e6eafa717d980b468f92ec67dd738453963e99515cc9ee40aeef502d1590465e8ba58d5@192.168.0.106:30301
 
 
